7d4845a292e80e60f2656665d0bc3e0dfd30e07c
Arshdeep Singh <singh.arshu021@gmail.com>
2026-06-06 00:27:32 +0530

    Day-1 uploaded

---
+++ Day-1/3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int factorialN(int n)
+{
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fact *= i;
+    }
+
+    return fact;
+}
+
+int main()
+{
+
+    cout << factorialN(5) << endl;
+    cout << factorialN(8) << endl;
+
+    return 0;
+}

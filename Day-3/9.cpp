900cbe3fc7b8ef8775d2b06fa6e410a3e040d691
Arshdeep Singh <singh.arshu021@gmail.com>
2026-06-06 00:43:42 +0530

    Day-3 uploaded

---
+++ Day-3/9.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 31, count = 0;
+
+    if (n <= 1 || ((n > 2) && (n % 2 == 0)))
+        cout << n << " is not prime";
+    else
+    {
+        if (n == 2)
+        {
+            cout << n << " is prime";
+            return 0;
+        }
+        else
+        {
+
+            for (int i = 3; i * i <= 3; i += 2)
+            {
+                if (n % i == 0)
+                    count++;
+            }
+
+            if (count > 0)
+                cout << n << " is not prime";
+            else
+                cout << n << " is prime";
+        }
+    }
+    return 0;
+}

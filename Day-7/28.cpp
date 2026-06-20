b2c49cdca205bddab782ddca9087e9c5eb1d8295
Arshdeep Singh <singh.arshu021@gmail.com>
2026-06-21 01:14:17 +0530

    Day-7 uploaded

---
+++ Day-7/28.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int reverseNum(int n, int rev = 0)
+{
+    if (n == 0)
+        return rev;
+    return reverseNum(n / 10, rev * 10 + (n % 10));
+}
+
+int main()
+{
+    int num = 6231;
+    cout << "Reversed number = " << reverseNum(num);
+    return 0;
+}

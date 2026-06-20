5cb72eb50d944f10b74f44356eb1b721a3aef84b
Arshdeep Singh <singh.arshu021@gmail.com>
2026-06-21 02:05:09 +0530

    Day-3 uploaded

--- Day-3/9.cpp
+++ Day-3/9.cpp
@@ -17,7 +17,7 @@ int main()
         else
         {
 
-            for (int i = 3; i * i <= 3; i += 2)
+            for (int i = 3; i * i <= n; i += 2)
             {
                 if (n % i == 0)
                     count++;
